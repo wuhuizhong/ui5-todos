12fb94f667c6ee34eb8f8dfb50b408a57c9e1776
Corey Wu <whz110@163.com>
2017-08-01 02:46:22 +0000

    Step 21: Data Types

---
+++ public/webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,18 @@
+
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("webapp.controller.InvoiceList", {
+
+		onInit : function () {
+			var oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+		}
+
+	});
+});

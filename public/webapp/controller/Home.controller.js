fc909e3f3ad404d7b5ea6c3df044cd81fd174dfe
Corey Wu <whz110@163.com>
2017-08-10 01:15:53 +0000

    Display a Target Without Changing the Hash

--- public/webapp/controller/Home.controller.js
+++ public/webapp/controller/Home.controller.js
@@ -25,6 +25,18 @@ sap.ui.define([
       var win = window.open('/', '_blank');
       win.focus();
     },
+    
+    onDisplayNotFound : function (oEvent) {
+			// display the "notFound" target without changing the hash
+			// 点击按钮可以导航到NotFound
+		  // 也可以this.getOwnerComponent().getRouter().getTargets()
+		  // 或者this.getOwnerComponent().getTargets()获取router
+			// 在display方法第二个参数传一个data对象，里面包含了来源target
+			this.getRouter().getTargets().display("notFound", {
+				fromTarget : "home"
+			});
+			// 在NotFound controller 的init function中给此事件注册一个监听器
+		}
 
 
   });
--- public/webapp/controller/NotFound.controller.js
+++ public/webapp/controller/NotFound.controller.js
@@ -4,6 +4,27 @@ sap.ui.define([
    "use strict";
    return BaseController.extend("webapp.controller.NotFound", {
       onInit: function () {
-      }
+         //给notFound target的 display事件注册一个监听器
+         var oRouter, oTarget;
+			oRouter = this.getRouter();
+			oTarget = oRouter.getTarget("notFound");
+			oTarget.attachDisplay(function (oEvent) {
+				this._oData = oEvent.getParameter("data"); //获取前面传入的data对象
+			}, this);
+      },
+		// override the parent's onNavBack (inherited from BaseController)
+		onNavBack : function (oEvent){
+			var oHistory, sPreviousHash, oRouter;
+			// in some cases we could display a certain target when the back button is pressed
+			// 如果这个data对象有值，则通过display的方法来显示，并删除对象里面的值。
+			if (this._oData && this._oData.fromTarget) {
+				this.getRouter().getTargets().display(this._oData.fromTarget);
+				delete this._oData.fromTarget;
+				return;
+			}
+			// call the parent's onNavBack
+			BaseController.prototype.onNavBack.apply(this, arguments);
+		}
+		
    });
 });

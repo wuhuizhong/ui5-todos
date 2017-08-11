885df8800ea87ffeabf1fd8ca2c2c719b0018241
Corey Wu <whz110@163.com>
2017-08-11 07:30:22 +0000

    Assign Multiple Targets

--- public/webapp/controller/Home.controller.js
+++ public/webapp/controller/Home.controller.js
@@ -40,6 +40,9 @@ sap.ui.define([
 		
 		onNavToEmployees : function (oEvent){
 			this.getRouter().navTo("employeeList");
+		},
+		onNavToEmployeeOverview : function (oEvent) {
+			this.getRouter().navTo("employeeOverview");
 		}
 
 
---
+++ public/webapp/controller/walkthrough/employee/EmployeeOverview.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"webapp/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+	return BaseController.extend("webapp.controller.walkthrough.employee.EmployeeOverview", {
+	});
+});

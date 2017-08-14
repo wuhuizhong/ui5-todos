db9d3e66a4ac998aa8302165c30796ec6b43ab23
Corey Wu <corey.wu@qq.com>
2017-08-15 00:03:10 +0800

    TasksWithListId

--- public/webapp/controller/todos/TasksWithListId.controller.js
+++ public/webapp/controller/todos/TasksWithListId.controller.js
@@ -11,29 +11,29 @@ sap.ui.define([
   "use strict";
 
   var CController = Controller.extend("webapp.controller.todos.TasksWithListId", {
-      
+
       oTasks: Mongo.Collection.get("Tasks"),
-    
+
       onInit: function() {
         this._oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-        
+
         // Set up local model for view state
         const viewState = {Task: {}};
         var oViewStateModel = new JSONModel(viewState);
         this.getView().setModel(oViewStateModel, "viewState");
-        
+
         // Set up route handling
         this._oRouter.attachRoutePatternMatched(this._onRoutePatternMatched, this);
-        
+
         // Include our custom style sheet
         jQuery.sap.includeStyleSheet("webapp/style.css");
-        
+
         // Subscribe to tasks data.
         // this._subscription = Meteor.subscribe('tasks');
         this._subscription = Meteor.subscribe("tasks", () =>{
           this._loadTasksForCurrentRoute();
         });
-        
+
       },
 
       onExit: function(){
@@ -43,12 +43,12 @@ sap.ui.define([
       _onRoutePatternMatched: function(oEvent) {
         // Store current route name and view state model
         this._sRouteName = oEvent.mParameters.name;
-  
+
         // Set which view and source files to display in our view state model
         if (this._sRouteName === "tasksWithListId"){
           var oArguments = oEvent.getParameters().arguments;
           this._sListId = oArguments.listId;
-          this._loadTasksForCurrentRoute();
+          // this._loadTasksForCurrentRoute();
         }
       },
 
@@ -57,21 +57,28 @@ sap.ui.define([
         if (!this._sListId){
           return;
         }
-  
+
         // Get tasks
         // var oTask = Mongo.Collection.get("Tasks").find({"listId": this._sListId});
-        var oTask = Mongo.Collection.get("Tasks").find({"listId": this._sListId});
+        var oTask = Mongo.Collection.get("Tasks").find();
         // meteor mongo
         // db.Tasks.find({"listId" : "n2sYSzdpwm4hAN5eo"})
         console.log("this._sListId: " + this._sListId);
-        console.log("oTask: " + oTask.count());
+        // console.log("oTask: " + oTask.count());
         if (!oTask){
           return;
         }
-  
+
         // Store in view model for view property binding
         const oModel = this.getView().getModel("viewState");
         oModel.setProperty("/Task", oTask);
+        console.log(oModel.getData());
+        // var localdata = oModel.getData();
+        // var jsonString = JSON.stringfy(localdata
+        // https://stackoverflow.com/questions/34902699/how-to-wait-for-a-jsonmodel-loaddata-request-in-sapui5-openui5/34925614
+        // oModel.attachRequestCompleted(function() {
+        //   console.log(oModel.getData());
+        // });
       },
 
       onAddTask: function(oEvent){
@@ -86,7 +93,7 @@ sap.ui.define([
       onSelectionChange: function(oEvent){
         var oListItem = oEvent.getParameters().listItem;
         var oTaskData = oListItem.getBindingContext().getObject();
-  
+
         // Set the checked property in the database to match the current selection
         this.oTasks.update(oTaskData._id, {
           $set: { checked: oListItem.getSelected() },
@@ -100,12 +107,12 @@ sap.ui.define([
           return sText;
         }
       },
-      
+
       onPressShowCompleted: function(){
         // Get current state of "show completed" toggle button
         var oViewState = this.getView().getModel('ViewState');
         var bShowCompleted = oViewState.getProperty('/showCompleted');
-    
+
         // Build task filter according to current state
         var aFilters = [];
         if (!bShowCompleted){
@@ -115,7 +122,7 @@ sap.ui.define([
               value1: true
           }));
         }
-    
+
         // Set filter
         var oTaskList = this.byId("TaskList");
         oTaskList.getBinding('items').filter(aFilters);
@@ -124,7 +131,7 @@ sap.ui.define([
       onPressDeleteTask: function(oEvent){
         var oListItem = oEvent.getSource();
         var oTaskData = oListItem.getBindingContext().getObject();
-      
+
         // Ask user to confirm delete
         var that = this;
         MessageBox.confirm("Permanently remove task?", {
